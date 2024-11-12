f161b43652604f88bb389aaa77e8be96ac10a0ce
usioZero <swortaartonlineforever@gmail.com>
2024-11-12 23:04:52 +0100

    minor fix

--- src/main/java/flowershop/controllers/FlowerShopController.java
+++ src/main/java/flowershop/controllers/FlowerShopController.java
@@ -3,31 +3,32 @@ package flowershop.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.javamoney.moneta.Money;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import flowershop.catalogs.ProductCatalog;
 import flowershop.models.embedded.Pricing;
 import flowershop.models.product.Flower;
 
 @Controller
 public class FlowerShopController {
+
+    private final ProductCatalog productCatalog;
+
+    FlowerShopController(ProductCatalog productCatalog){
+        this.productCatalog = productCatalog;
+    }
+
     @GetMapping("/sell")
     public String sell(Model model){
-
-        List<Flower> flowers = new ArrayList<>();
-        flowers.addAll(List.of(
-            new Flower("Rose", new Pricing(Money.of(2, "EUR") , Money.of(5, "EUR")), "Red"),
-            new Flower("Lilac", new Pricing(Money.of(3, "EUR") , Money.of(9, "EUR")), "White"),
-            new Flower("Iris", new Pricing(Money.of(1.3, "EUR") , Money.of(4.5, "EUR")), "Purple"))
-            
-        );
-        model.addAttribute("flowers", flowers);
+        model.addAttribute("flowers", productCatalog.findAll().filter(product -> product instanceof Flower).toList());
         
         List<String> typeList = new ArrayList<>();
-        typeList.addAll(List.of("Red", "White", "Purple"));
+        typeList.addAll(List.of("Red", "Yellow", "White"));
         model.addAttribute("typeList", typeList);
 
         String selectedItem = "";
