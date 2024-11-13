839970c3d3fdbfd3c0c465dc5d9dbeb076400a1d
UsioZero <swortaartonlineforever@gmail.com>
2024-11-13 18:32:38 +0100

    minor fix

--- src/main/java/flowershop/controllers/FlowerShopController.java
+++ src/main/java/flowershop/controllers/FlowerShopController.java
@@ -1,6 +1,7 @@
 package flowershop.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,24 +23,26 @@ public class FlowerShopController {
     }
 
     @GetMapping("/sell")
-    public String sell(Model model){
+    public String sell(Model model, @RequestParam(value = "color", required = false) String selectedItem){
 
         List<Flower> flowers = productCatalog.findAll()
         .filter(product -> product instanceof Flower)
         .map(product -> (Flower) product).toList();
 
+        if (selectedItem != null && !selectedItem.isEmpty()) {
+            flowers = flowers.stream()
+                    .filter(flower -> flower.getColor().equalsIgnoreCase(selectedItem))
+                    .collect(Collectors.toList());
+        }
+
         Set<String> colors = flowers.stream()
         .map(Flower::getColor)
         .collect(Collectors.toSet());
 
-        model.addAttribute("flowers", flowers);
         
-        List<String> typeList = new ArrayList<>();
-        typeList.addAll(colors);
-        model.addAttribute("typeList", typeList);
-
-        String selectedItem = "";
+        model.addAttribute("typeList", colors);
         model.addAttribute("selectedItem", selectedItem);
+        model.addAttribute("flowers", flowers);
 
         return "sell";
     }
