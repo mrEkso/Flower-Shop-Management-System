e9170afc3b0baf231084fd1599a18b0f3e133650
usioZero <swortaartonlineforever@gmail.com>
2024-11-18 17:37:15 +0100

    add busket to sell and buy pages

---
+++ src/main/java/flowershop/sales/BasketItem.java
@@ -0,0 +1,37 @@
+package flowershop.sales;
+
+import flowershop.product.Flower;
+
+public class BasketItem {
+    private Flower flower;
+    private int quantity;
+
+    public BasketItem(Flower flower, int quantity) {
+        this.flower = flower;
+        this.quantity = quantity;
+    }
+
+    public Flower getFlower() {
+        return flower;
+    }
+
+    public void setFlower(Flower flower) {
+        this.flower = flower;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void increaseQuantity() {
+        this.quantity++;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return this.flower.getPricing().getSellPrice().getNumber().doubleValue();
+    }
+}
--- src/main/java/flowershop/sales/SalesController.java
+++ src/main/java/flowershop/sales/SalesController.java
@@ -2,10 +2,14 @@ package flowershop.sales;
 
 import flowershop.product.Flower;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,7 +17,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import flowershop.product.ProductCatalog;
-
+import jakarta.servlet.http.HttpServletRequest;
+@SessionAttributes("basket")
 @Controller
 public class SalesController {
 
@@ -22,17 +27,23 @@ public class SalesController {
         this.productCatalog = productCatalog;
     }
 
+    @ModelAttribute("basket")
+    public List<BasketItem> createBasket() {
+        return new ArrayList<>();
+    }
+
     @GetMapping("/sell")
     public String sell(Model model, 
         @RequestParam(required = false) String filterItem, 
-        @RequestParam(required = false) String searchInput) {
+        @RequestParam(required = false) String searchInput,
+        @ModelAttribute("basket") List<BasketItem> basket) {
     
         List<Flower> flowers = productCatalog.findAll()
             .filter(product -> product instanceof Flower)
             .map(product -> (Flower) product)
             .toList();
         
-        // System.out.println(filterItem);
+        System.out.println(basket);
 
         if (filterItem != null && !filterItem.isEmpty()) {
             flowers = flowers.stream()
@@ -56,6 +67,7 @@ public class SalesController {
         model.addAttribute("filterItem", filterItem);
         model.addAttribute("searchInput", searchInput);
         model.addAttribute("flowers", flowers);
+        model.addAttribute("basket", basket);
     
         return "sell";
     }
@@ -63,39 +75,85 @@ public class SalesController {
     @GetMapping("/buy")
     public String buy(Model model,
         @RequestParam(required = false) String filterItem, 
-        @RequestParam(required = false) String searchInput) {
-
-        List<Flower> flowers = productCatalog.findAll()
+        @RequestParam(required = false) String searchInput,
+        @ModelAttribute("basket") List<BasketItem> basket) {
+    
+            List<Flower> flowers = productCatalog.findAll()
+                .filter(product -> product instanceof Flower)
+                .map(product -> (Flower) product)
+                .toList();
+            
+            System.out.println(basket);
+    
+            if (filterItem != null && !filterItem.isEmpty()) {
+                flowers = flowers.stream()
+                        .filter(flower -> flower.getColor().equalsIgnoreCase(filterItem))
+                        .toList();
+            }
+        
+            if (searchInput != null && !searchInput.isEmpty()) {
+                flowers = flowers.stream()
+                        .filter(flower -> flower.getName().toLowerCase().contains(searchInput.toLowerCase()))
+                        .toList();
+            }
+    
+            Set<String> colors = productCatalog.findAll()
             .filter(product -> product instanceof Flower)
             .map(product -> (Flower) product)
-            .toList();
+            .toList().stream().map(Flower::getColor)
+            .collect(Collectors.toSet());
         
-        // System.out.println(filterItem);
-
-        if (filterItem != null && !filterItem.isEmpty()) {
-            flowers = flowers.stream()
-                    .filter(flower -> flower.getColor().equalsIgnoreCase(filterItem))
-                    .toList();
-        }
-
-        if (searchInput != null && !searchInput.isEmpty()) {
-            flowers = flowers.stream()
-                    .filter(flower -> flower.getName().toLowerCase().contains(searchInput.toLowerCase()))
-                    .toList();
-        }
-
-        Set<String> colors = productCatalog.findAll()
-        .filter(product -> product instanceof Flower)
-        .map(product -> (Flower) product)
-        .toList().stream().map(Flower::getColor)
-        .collect(Collectors.toSet());
-
-        model.addAttribute("typeList", colors);
-        model.addAttribute("filterItem", filterItem);
-        model.addAttribute("searchInput", searchInput);
-        model.addAttribute("flowers", flowers);
+            model.addAttribute("typeList", colors);
+            model.addAttribute("filterItem", filterItem);
+            model.addAttribute("searchInput", searchInput);
+            model.addAttribute("flowers", flowers);
+            model.addAttribute("basket", basket);
 
         return "buy";
+    }
+
+    @PostMapping("/add-to-basket")
+    public String addToBasket(
+        Model model,
+        @RequestParam String flowerName, 
+        @RequestParam(required = false) String redirectPage,
+        @ModelAttribute("basket") List<BasketItem> basket
+    ) {
+        Flower flower = productCatalog.findAll()
+            .filter(product -> product instanceof Flower)
+            .map(product -> (Flower) product)
+            .filter(f -> f.getName().equalsIgnoreCase(flowerName))
+            .stream()
+            .findFirst()
+            .orElse(null);
+
+        Optional<BasketItem> basketItem = basket.stream()
+        .filter(item -> item.getFlower().equals(flower))
+        .findFirst();
+
+        if (flower != null) {
+            if(basketItem.isPresent()){
+                basketItem.get().increaseQuantity();
+            } else {
+                basket.add(new BasketItem(flower, 1));
+            }
+        }
+
+        model.addAttribute("basket", basket);
+
+        return "redirect:/" + redirectPage;
+    }
+
+    @PostMapping("/remove-from-basket")
+    public String removeFromBasket(
+        @RequestParam String flowerName,
+        @ModelAttribute("basket") List<BasketItem> basket,
+        HttpServletRequest request
+    ) {
+        String referer = request.getHeader("Referer").split("http://localhost:8080/")[1];
+        basket.removeIf(b -> b.getFlower().getName().equalsIgnoreCase(flowerName));
+        
+        return "redirect:/" + (referer == null? "/sell": referer);
     }
 
     @GetMapping("/")
