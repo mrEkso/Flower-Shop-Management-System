46ae9043a7c0dc3cb874350653859346c49f5aac
bechir <bechirsayadi01@gmail.com>
2024-11-20 00:37:40 +0100

    worked on inventory and quite finished

---
+++ src/main/java/flowershop/product/Product.java
@@ -0,0 +1,53 @@
+package flowershop.product;
+
+public class Product {
+	private String name;
+	private String type; // "Flower" or "Bouquet"
+	private int quantity;
+	private double pricePerUnit;
+
+	public Product(String name, String type, int quantity, double pricePerUnit) {
+		this.name = name;
+		this.type = type;
+		this.quantity = quantity;
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	public Product(Flower flower) {
+		this.name = flower.getName();
+		this.type = "Flower";
+		this.quantity = flower.getQuantity();
+		this.pricePerUnit = (flower.getPricing() != null && flower.getPricing().getSellPrice() != null)
+			? flower.getPricing().getSellPrice().getNumber().doubleValueExact()
+			: 0.0;
+	}
+
+	public Product(Bouquet bouquet) {
+		this.name = bouquet.getName();
+		this.type = "Bouquet";
+		this.quantity = 1;
+		this.pricePerUnit = (bouquet.getPricing() != null && bouquet.getPricing().getSellPrice() != null)
+			? bouquet.getPricing().getSellPrice().getNumber().doubleValueExact()
+			: 0.0;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public double getPricePerUnit() {
+		return pricePerUnit;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
