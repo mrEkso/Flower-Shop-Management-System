4abbb27119fba6a18a3111282ecb64a924c02b9f
bechir <bechirsayadi01@gmail.com>
2024-11-20 18:26:13 +0100

    inventory and products

--- src/main/java/flowershop/inventory/DeletedProductService.java
+++ src/main/java/flowershop/inventory/DeletedProductService.java
@@ -8,18 +8,14 @@ import java.util.List;
 @Service
 public class DeletedProductService {
 
-	public DeletedProduct deleteProduct(Product product, int quantityDeleted) {
-		return new DeletedProduct(product.getName(),
-			product.getPrice().getNumber().doubleValueExact(),
-			quantityDeleted,
-			product.getPrice().getNumber().doubleValueExact() * quantityDeleted);
+	public DeletedProduct deleteProduct(Product product, int quantity) {
+		double loss = product.getPrice().getNumber().doubleValueExact() * quantity;
+		return new DeletedProduct(product.getName(), product.getPrice().getNumber().doubleValueExact(), quantity, loss);
 	}
 
-	// TODO: deletedProductList should be stored in a catalog or repository but it's okay for now.
-	public double getTotalLossSum (List<DeletedProduct> deletedProductList){
-		return deletedProductList.stream()
+	public double getTotalLossSum(List<DeletedProduct> deletedProducts) {
+		return deletedProducts.stream()
 			.mapToDouble(DeletedProduct::getTotalLoss)
 			.sum();
 	}
-
 }
