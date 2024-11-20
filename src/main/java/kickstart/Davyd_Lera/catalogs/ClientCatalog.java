33b2e0e67402b157a4f2c5ce7f8b1c8e0c2fb083
Vadym Zaitsev <fadim7348@gmail.com>
2024-11-20 22:42:43 +0100

    removed kickstart.Davyd_Lera

--- src/main/java/kickstart/Davyd_Lera/catalogs/ClientCatalog.java
+++
@@ -1,8 +0,0 @@
-package kickstart.Davyd_Lera.catalogs;
-
-import kickstart.Davyd_Lera.models.Client;
-import org.salespointframework.inventory.Inventory;
-import org.springframework.data.repository.CrudRepository;
-
-public class ClientCatalog extends CrudRepository<Client, CustomerIdentifier> {
-}
--- src/main/java/kickstart/Davyd_Lera/models/payment/CardPayment.java
+++
@@ -1,9 +0,0 @@
-package kickstart.Davyd_Lera.models.payment;
-
-import org.salespointframework.payment.PaymentMethod;
-
-public class CardPayment extends PaymentMethod {
-	public CardPayment() {
-		super("Credit Card");
-	}
-}
