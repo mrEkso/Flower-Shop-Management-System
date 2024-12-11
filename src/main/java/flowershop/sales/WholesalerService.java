600464adf3ddbf1e66f1012b1291300c3021006f
DeeWan17 <borodaev17@gmail.com>
2024-12-11 22:12:19 +0100

    New tests for SalesService and minor cleanup.

--- src/main/java/flowershop/sales/WholesalerService.java
+++ src/main/java/flowershop/sales/WholesalerService.java
@@ -1,47 +1,47 @@
-package flowershop.sales;
-
-import flowershop.product.Flower;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Service
-public class WholesalerService {
-
-	private final WholesalerRepository wholesalerRepository;
-
-	@Autowired
-	public WholesalerService(WholesalerRepository wholesalerRepository) {
-		this.wholesalerRepository = wholesalerRepository;
-	}
-
-	public List<Flower> findAllFlowers() {
-		return (List<Flower>) wholesalerRepository.findAll(); // Retrieve all flowers from the repository
-	}
-
-
-	public List<Flower> findFlowersByColor(String color) {
-		return wholesalerRepository.findByColorIgnoreCase(color);
-	}
-
-	public List<Flower> findFlowersByName(String subString) {
-		return wholesalerRepository.findByNameIgnoreCase(subString);
-	}
-
-	public Set<String> findAllFlowerColors() {
-		return findAllFlowers()
-			.stream()
-			.map(Flower::getColor)
-			.collect(Collectors.toSet());
-	}
-}
-
+//package flowershop.sales;
+//
+//import flowershop.product.Flower;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//@Service
+//public class WholesalerService {
+//
+//	private final WholesalerRepository wholesalerRepository;
+//
+//	@Autowired
+//	public WholesalerService(WholesalerRepository wholesalerRepository) {
+//		this.wholesalerRepository = wholesalerRepository;
+//	}
+//
+//	public List<Flower> findAllFlowers() {
+//		return (List<Flower>) wholesalerRepository.findAll(); // Retrieve all flowers from the repository
+//	}
+//
+//
+//	public List<Flower> findFlowersByColor(String color) {
+//		return wholesalerRepository.findByColorIgnoreCase(color);
+//	}
+//
+//	public List<Flower> findFlowersByName(String subString) {
+//		return wholesalerRepository.findByNameIgnoreCase(subString);
+//	}
+//
+//	public Set<String> findAllFlowerColors() {
+//		return findAllFlowers()
+//			.stream()
+//			.map(Flower::getColor)
+//			.collect(Collectors.toSet());
+//	}
+//}
+//
