52fd25fe69b170c95013f8613327e3d85bf9200a
usioZero <swortaartonlineforever@gmail.com>
2024-12-16 23:48:43 +0100

    bigfix inventory

--- src/main/java/flowershop/inventory/InventoryController.java
+++ src/main/java/flowershop/inventory/InventoryController.java
@@ -7,7 +7,6 @@ import flowershop.product.ProductService;
 import org.javamoney.moneta.Money;
 import org.salespointframework.catalog.Product;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
