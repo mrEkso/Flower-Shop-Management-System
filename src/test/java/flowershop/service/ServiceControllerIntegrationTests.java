e0d59417e3eef86fc369ae329a298a7e30a3c537
usioZero <swortaartonlineforever@gmail.com>
2025-01-12 23:41:19 +0100

    minor fix

--- src/test/java/flowershop/service/ServiceControllerIntegrationTests.java
+++ src/test/java/flowershop/service/ServiceControllerIntegrationTests.java
@@ -224,7 +224,7 @@ class ServiceControllerIntegrationTests {
 				.param("notes", "Test notes")
 				.param("deliveryPrice", "50"))
 			.andExpect(status().is3xxRedirection())
-			.andExpect(redirectedUrl("/services/create"));
+			.andExpect(redirectedUrl("/services"));
 	}
 
 	@Test
@@ -286,7 +286,7 @@ class ServiceControllerIntegrationTests {
 				.param("phone", "123456789")
 				.param("notes", "Test notes"))
 			.andExpect(status().is3xxRedirection())
-			.andExpect(redirectedUrl("/services/create"));
+			.andExpect(redirectedUrl("/services"));
 	}
 
 	@Test
