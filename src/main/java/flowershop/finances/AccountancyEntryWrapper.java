e1d4e8e02eac2d45da9f92c24f8589a4cd021bac
Vadym Zaitsev <fadim7348@gmail.com>
2025-01-13 17:24:51 +0100

    Imma head out (To Kaufland)

--- src/main/java/flowershop/finances/AccountancyEntryWrapper.java
+++ src/main/java/flowershop/finances/AccountancyEntryWrapper.java
@@ -10,11 +10,6 @@ import flowershop.services.ContractOrder;
 import flowershop.services.EventOrder;
 import flowershop.services.ReservationOrder;
 import jakarta.persistence.*;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.salespointframework.accountancy.AccountancyEntry;
 import org.salespointframework.catalog.Product;
 import org.salespointframework.order.ChargeLine;
@@ -22,22 +17,12 @@ import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderLine;
 import org.salespointframework.order.Totalable;
 import org.salespointframework.quantity.Quantity;
-import org.vandeseer.easytable.TableDrawer;
-import org.vandeseer.easytable.settings.HorizontalAlignment;
-import org.vandeseer.easytable.structure.Row;
-import org.vandeseer.easytable.structure.Table;
-import org.vandeseer.easytable.structure.cell.TextCell;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static flowershop.finances.FinancialReport.emptyRow;
 
 
 /**
@@ -64,13 +49,45 @@ public class AccountancyEntryWrapper extends AccountancyEntry {
 	private Category category;
 	private LocalDateTime timestamp;
 	private String clientName;
+	private String clientPhone;
+	private String adress;
+	private LocalDateTime date1;
+	private LocalDateTime date2;
+	private String frequency;
 	private LocalDate reservationExecution;
+	private String notes;
+	private String paymentMethod;
+
 
 	@Transient
 	private ProductService productService;
-	@Transient
-	private ClockService clockService;
 
+
+	public String getClientPhone(){
+		if(clientPhone != null) return clientPhone;
+		return "";
+	}
+	public String getAdress(){
+		if(adress != null) return adress;
+		return "";
+	}
+	public LocalDateTime getDate1(){
+		if(date1 != null) return date1;
+		return null;
+	}
+	public LocalDateTime getDate2(){
+		if(date2 != null) return date2;
+		return null;
+	}
+	public String getFrequency(){
+		if(frequency != null) return frequency;
+		return "";
+	}
+
+	public String getNotes() {
+		if(notes != null) return notes;
+		return "";
+	}
 
 	/**
 	 *
@@ -141,23 +158,41 @@ public class AccountancyEntryWrapper extends AccountancyEntry {
 		super(order.getTotal());
 		this.productService = productService;
 		this.timestamp = time;
+		this.paymentMethod = order.getPaymentMethod().toString();
 		if (order instanceof WholesalerOrder) {
 			this.category = Category.Einkauf;
 			String notes = ((WholesalerOrder) order).getNotes();
 			if(notes != null) {
 				this.reservationExecution = LocalDate.parse(notes);
+				this.date1 = this.reservationExecution.atTime(9,0);
+			}
+			else{
+				this.date1 = ClockService.nextWorkingDay(this.timestamp.toLocalDate()).atTime(9, 0);
 			}
 		} else if (order instanceof ContractOrder) {
 			this.clientName = ((ContractOrder) order).getClient().getName();
+			this.clientPhone = ((ContractOrder) order).getClient().getPhone();
+			this.adress = ((ContractOrder) order).getAddress();
 			this.category = Category.Vertraglicher_Verkauf;
+			this.date1 = ((ContractOrder) order).getStartDate();
+			this.date2 = ((ContractOrder) order).getEndDate();
+			this.frequency = ((ContractOrder) order).getFrequency();
+			this.notes = ((ContractOrder) order).getNotes();
 		} else if (order instanceof EventOrder) {
 			this.clientName = ((EventOrder) order).getClient().getName();
+			this.clientPhone = ((EventOrder) order).getClient().getPhone();
+			this.adress = ((EventOrder) order).getDeliveryAddress();
 			this.category = Category.Veranstaltung_Verkauf;
-			this.reservationExecution = ((EventOrder) order).getEventDate().toLocalDate();
+			this.date1 = ((EventOrder) order).getEventDate();
+			this.reservationExecution = this.date1.toLocalDate();
+			this.notes = ((EventOrder) order).getNotes();
 		} else if (order instanceof ReservationOrder) {
 			this.clientName = ((ReservationOrder) order).getClient().getName();
+			this.clientPhone = ((ReservationOrder) order).getClient().getPhone();
 			this.category = Category.Reservierter_Verkauf;
-			this.reservationExecution = ((ReservationOrder) order).getReservationDateTime().toLocalDate();
+			this.date1 = ((ReservationOrder) order).getReservationDateTime();
+			this.reservationExecution = this.date1.toLocalDate();
+			this.notes = ((ReservationOrder) order).getNotes();
 		} else if (order instanceof SimpleOrder) {
 			this.category = Category.Einfacher_Verkauf;
 		} else {
@@ -201,66 +236,5 @@ public class AccountancyEntryWrapper extends AccountancyEntry {
 	public Map<Product, Quantity> getFlowers(){
 		return productQuantityMap;
 	}
-
-	public byte[] generatePDF() {
-		try (PDDocument document = new PDDocument()) {
-			InputStream inFont = getClass().getResourceAsStream("/fonts/josefin-sans.semibold.ttf");
-			System.out.println(inFont.toString());
-			PDType0Font customFont = PDType0Font.load(document, inFont);
-
-			TableDrawer.builder()
-				.startX(50)
-				.endY(50)
-				.startY(780)
-				.table(buildTheTable(customFont))
-				.build()
-				.draw(() -> document, () -> new PDPage(PDRectangle.A4), 50);
-			try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-				document.save(outputStream);
-				return outputStream.toByteArray();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private Table buildTheTable(PDFont font) {
-		Table.TableBuilder builder = Table.builder()
-			.addColumnsOfWidth(110, 115, 110, 50, 45, 55);
-		Row shapka1 = Row.builder()
-			.add(TextCell.builder()
-				.text(" ").fontSize(16).colSpan(2)
-				.build())
-			.add(TextCell.builder()
-				.text("Floris Blumenladen Dresden").fontSize(16).colSpan(4).horizontalAlignment(HorizontalAlignment.LEFT).font(font)
-				.build())
-			.build();
-		builder.addRow(shapka1);
-		Row adress = Row.builder()
-			.add(TextCell.builder()
-				.text(" ").fontSize(16).colSpan(2)
-				.build())
-			.add(TextCell.builder()
-				.text("Wiener Platz 4, 01069 Dresden").fontSize(16).colSpan(4).horizontalAlignment(HorizontalAlignment.LEFT).font(font)
-				.build())
-			.build();
-		builder.addRow(adress);
-		LocalDateTime day = this.clockService.now();
-		String month = (day.getMonth().getValue() < 10) ? "0" + day.getMonth().getValue() : String.valueOf(day.getMonth().getValue());
-		String dateRepr = new StringBuilder().append(day.getDayOfMonth()).append(".").append(month).append(".").append(day.getYear()).toString();
-
-		Row datum = Row.builder()
-			.add(TextCell.builder()
-				.text(" ").fontSize(16).colSpan(2)
-				.build())
-			.add(TextCell.builder()
-				.text("Am " + dateRepr).fontSize(16).colSpan(4).horizontalAlignment(HorizontalAlignment.LEFT).font(font)
-				.build())
-			.build();
-		builder.addRow(datum);
-		builder.addRow(emptyRow());
-	}
-
 
 }
