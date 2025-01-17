6554b02f5e88065dcb15a16e1a4085bb3744182a
usioZero <swortaartonlineforever@gmail.com>
2025-01-17 16:27:06 +0100

    minor fix

--- src/main/java/flowershop/product/ProductService.java
+++ src/main/java/flowershop/product/ProductService.java
@@ -220,10 +220,8 @@ public class ProductService {
 	public void updateSellPrice(Product product, double newSellPrice) {
 		if (product instanceof Flower flower) {
 			flower.getPricing().setSellPrice(Money.of(newSellPrice, "EUR"));
-			flower.setPrice(flower.getPricing().getSellPrice());
 		} else if (product instanceof Bouquet bouquet) {
 			bouquet.setAdditionalPrice(Money.of(newSellPrice, "EUR"));
-			bouquet.setPrice(bouquet.getPricing().getSellPrice());
 		} else {
 			throw new IllegalStateException("Unsupported product type: " + product.getClass().getSimpleName());
 		}
